5afd310b0608e857897e7b481a47c6bc590d5e8f
likhe <likhelifi@laposte.net>
2017-05-08 17:21:40 +0200

    correct answers display successfully

--- ex/quizz/app.js
+++ ex/quizz/app.js
@@ -4,9 +4,30 @@
 let button = document.getElementById("button");
 //the answer
 let good = document.getElementById("after_submit");
-function valid(e) {
+let text = document.getElementById("number_correct");
+function valid() {
+  //the questions
+  let one = document.quizz.question1.value ;
+  let two = document.quizz.question2.value ;
+  let three = document.quizz.question3.value ;
+  //the correct answers
+  let correct = 0;
+  //if the user have the good answers
+if (one=="Linda") {
+  correct = correct + 1;
+}
+if (two=="female") {
+  correct = correct + 1;
+}
+if (three=="marseille") {
+  correct = correct + 1;
+}
+console.log(correct);
   //show me the answers when it's good
   good.style.visibility = "visible";
+  //write in the paragraph
+  text.innerHTML = "Tu as " + correct + " bonnes réponses";
+
 
 }
 button.addEventListener("click", valid, false);
